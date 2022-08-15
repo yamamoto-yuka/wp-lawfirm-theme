4d3a082c176bd1a10b5e837731c2cc0dca99e9c0
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2022-08-14 19:47:43 -0700

    Modifued singpage

--- single-team.php
+++ single-team.php
@@ -4,8 +4,18 @@
     <section class="elementor-section elementor-top-section elementor-element elementor-section-boxed elementor-section-height-default elementor-section-height-default single-team" data-element_type="section">
       <div class="elementor-container elementor-column-gap-default">
         <div class="elementor-column elementor-col-30 elementor-top-column elementor-element" data-element_type="column">
-          <div class="elementor-widget-wrap elementor-container elementor-column-gap-default">
-                <?php echo the_post_thumbnail('large', array('class' => 'img-fluid')); ?>
+          <div class="elementor-widget-wrap elementor-container elementor-column-gap-default contact-links">
+            <?php echo the_post_thumbnail('large', array('class' => 'img-fluid')); ?>
+            <?php 
+              $email = get_field('email_address');
+              $phone = get_field('phone_number');
+              if($email != ''):
+                echo '<a href="mailto:'.$email.'" title="Email '.get_the_title().'"><i class="fas fa-envelope-square"></i></a>';
+              endif;
+              if($phone !=''):
+                echo '<a href="tel:'.$phone.'" title="Call '.get_the_title().'"><i class="fas fa-phone-square"></i></a>';
+              endif;
+            ?>
           </div>
         </div>
         <div class="elementor-column elementor-col-70 elementor-top-column elementor-element" data-element_type="column">
@@ -13,15 +23,31 @@
             <h2><?php echo get_the_title(); ?></h2>
             <h3><?php echo do_shortcode('[acf field="title"]'); ?></h3>
             <?php echo get_the_content(); ?>
-            <?php 
-                $areas = get_field('areas_of_practice');
-                $association = get_field('professional_associations');
-                $education = get_field('education');
-                if($areas != ''):
-                  echo '<h3 class="text-center">Areas of Expertise</h3>';
-                  echo '<div class="team-section">'.$areas.'</div>';
-                endif;
-            ?>
+            <div class="team-member-moreinfo">
+              <?php
+              $areas = get_field('areas_of_practice');
+              $association = get_field('professional_association');
+              $education = get_field('education');
+              if ($areas != '') :
+                echo '<div>';
+                echo '<h3>Areas of Expertize</h3>';
+                echo '<div class="team-section">' . $areas . '</div>';
+                echo '</div>';
+              endif;
+              if ($association != '') :
+                echo '<div>';
+                echo '<h3>Professional Association</h3>';
+                echo '<div class="team-section">' . $association . '</div>';
+                echo '</div>';
+              endif;
+              if ($education != '') :
+                echo '<div>';
+                echo '<h3>Education</h3>';
+                echo '<div class="team-section">' . $education . '</div>';
+                echo '</div>';
+              endif;
+              ?>
+            </div>
           </div>
         </div>
       </div>
